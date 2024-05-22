6acc307d5ded5f2127a1c5fe0daf3cff452298fb
DESKTOP-9RMPRFS\msn <dave.amor@gmail.com>
2024-05-22 16:37:37 +0100

    footer of login now says BloomQuote 2024

--- login.php
+++ login.php
@@ -102,7 +102,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div class="text-muted">Copyright &copy; BloomQuote 2024</div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;

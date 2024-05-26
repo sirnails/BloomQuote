9fe8e2dffc1a838027c32406fc6da93282d3fb67
DESKTOP-9RMPRFS\msn <dave.amor@gmail.com>
2024-05-26 11:53:17 +0100

    footer float rather than absolute

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,7 +1,20 @@
 </div>
 
-<p style=" position: absolute; bottom: 0; left: 0; width: 100%; text-align: center;">Last updated 2024-05-24 11:45am</p>	
+<style>
+#copyright {
+	float: left;
+	padding-bottom: 10px;
+	padding-top: 10px;
+	text-align: center;
+	bottom: 0px;
+	width: 100%;
+}
+</style>
 
+<div id="copyright">
+    Website Last updated 2024-05-24 11:55am <br>
+    Moved Copyright footer to be floating at end of content rather than absolute.
+</div>
 
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>

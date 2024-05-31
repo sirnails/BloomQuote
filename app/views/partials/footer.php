347cd3fccf05cde9f009b5ae7187572ab0236b7f
DESKTOP-9RMPRFS\msn <dave.amor@gmail.com>
2024-05-31 10:44:35 +0100

    margin to zero

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -8,9 +8,14 @@
         text-align: center;
         background-color: rgba(255, 255, 255, 0.5); /* Semi-transparent background */
         width: 100%;
+        bottom: 0;
+        box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1); /* Subtle shadow for depth */
+        margin: 0; /* Ensure no margin is added */
+        padding: 10px 0; /* Adds padding to space out the text */
     	}
 
     #footer p {
+        margin: 0; /* Remove default margin */
         color: #000; /* Dark text color */
     }
 
@@ -29,7 +34,7 @@
 </style>
 <div class="flex-container">
 <div id="footer">Website Last updated 2024-05-29 23:00<br>
-Footer removed white space from code</div>
+Set Margins to 0</div>
 </div>
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>

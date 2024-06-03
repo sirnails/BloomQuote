cb1498c581a1ee87fb0217e65af1771c807b0ce2
DESKTOP-9RMPRFS\msn <dave.amor@gmail.com>
2024-06-04 00:57:52 +0100

    added require lines as autoload and namespace is not working :(

--- index.php
+++ index.php
@@ -51,9 +51,13 @@ if (empty($_SESSION['csrf_token'])) {
 
 try {
 //       require_once './config/database.php';
-//       require_once './app/controllers/UserController.php';
-//       require_once './app/controllers/QuoteController.php';
-//   require_once './app/helpers/InputHelper.php';
+       require_once './app/controllers/UserController.php';
+       require_once './app/controllers/QuoteController.php';
+       require_once './app/helpers/InputHelper.php';
+       require_once './app/helpers/SanitizationHelper.php';
+       require_once './app/models/Quote.php';
+       require_once './app/models/QuoteItem.php';
+       require_once './app/models/User.php';
 } catch (Exception $e) {
     echo 'Error: ',  $e->getMessage(), "\n";
 }

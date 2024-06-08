ccf80312153a688cd5fc2d7837a24adae05f47b9
DESKTOP-9RMPRFS\msn <dave.amor@gmail.com>
2024-06-08 11:04:00 +0100

    footer note

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -33,8 +33,8 @@
         }
 </style>
 <div class="flex-container">
-<div id="footer">Website Last updated 2024-06-06 01:07<br>
-Automated the removal of dev tools</div>
+<div id="footer">Website Last updated 2024-06-08 11:05<br>
+Sanitise changes</div>
 </div>
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
